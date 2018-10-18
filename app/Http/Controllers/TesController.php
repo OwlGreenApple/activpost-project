bf90727988285d2f4241c4ac80386e9557f2d635
PuspitaN <puspita.celebgramme@gmail.com>
2018-10-18 14:04:25 +0700

    schedule

--- app/Http/Controllers/TesController.php
+++ app/Http/Controllers/TesController.php
@@ -134,7 +134,7 @@ class TesController extends Controller
       //$instagram = $i->timeline->uploadPhoto($photo, ['caption' => $caption]);
       //$instagram = $i->timeline->uploadVideo($video->getFile(), ['caption' => $caption]);
       $caption = 'apayaaa';
-      $photo = new \InstagramAPI\Media\Photo\InstagramPhoto($photoFilename, ['targetFeed' => \InstagramAPI\Constants::FEED_STORY]);
+      //$photo = new \InstagramAPI\Media\Photo\InstagramPhoto($photoFilename, ['targetFeed' => \InstagramAPI\Constants::FEED_STORY]);
       $i->story->uploadPhoto($photoFilename, $metadata);
     }
 }
