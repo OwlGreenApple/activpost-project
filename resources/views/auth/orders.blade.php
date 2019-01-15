f66612a49a9aacb2d43cc1a8b24df04672e52c75
rizky redjo <celebgramme.dev@gmail.com>
2019-01-15 17:23:27 +0700

    fixing

--- resources/views/auth/orders.blade.php
+++ resources/views/auth/orders.blade.php
@@ -35,7 +35,7 @@
     <div class="row">
         <div class="col-md-12 col-xs-12 col-sm-12">
             <div class="panel panel-default">
-                <div class="panel-heading"><h1>Pilih Paket Berlangganan</h1></div>
+                <div class="panel-heading"><h1 align="center">Pilih Paket Berlangganan</h1></div>
                 <div class="panel-body">
 	
 									@if(Session::has('message'))
