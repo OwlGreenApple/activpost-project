cb0ce99e677d006a67d3a2ae0570bddfab961b78
rizky redjo <celebgramme.dev@gmail.com>
2019-02-15 17:05:51 +0700

    fixing

--- database/migrations/2018_12_04_111609_add_is_s3_to_schedules_table.php
+++ database/migrations/2018_12_04_111609_add_is_s3_to_schedules_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddIsS3ScheduleTable extends Migration
+class AddIsS3ToScheduleTable extends Migration
 {
     /**
      * Run the migrations.
