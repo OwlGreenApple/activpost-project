49c227a87509c391356740c833880b2851e18a81
rizky redjo <celebgramme.dev@gmail.com>
2019-02-18 16:54:44 +0700

    fixing

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -67,8 +67,9 @@ class APIController extends Controller
 					// $dir = public_path('images/uploads/'.$user->username.'-'.$user->id); 
 					// $dir = base_path('../public_html/dashboard/images/uploads/'.$user->username.'-'.$user->id); 
           if ($sc->is_s3) {
-            $fileContents = Storage::disk('s3')->get($sc->image);
-            $photo = Storage::disk('local')->put('post-temp/'.$user->username.'-'.$user->id.'/', $fileContents);
+            // $fileContents = Storage::disk('s3')->get($sc->image);
+            // $photo = Storage::disk('local')->put('post-temp/'.$user->username.'-'.$user->id.'/', $fileContents);
+            $photo = Storage::disk('s3')->download($sc->image);
           }
           else {
             $dir = base_path('../public_html/vp/uploads/'.$user->username.'-'.$user->id); 
