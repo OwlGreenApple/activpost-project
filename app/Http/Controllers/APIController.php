ac2fc5f44e3327f02edbdd9f54977ae30a245a68
rizky redjo <celebgramme.dev@gmail.com>
2019-02-19 10:29:33 +0700

    fixing

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -71,7 +71,7 @@ class APIController extends Controller
             $ext = substr(strrchr($fileContents,'.'),1);
             // if (!file_exists($dir)) {
             // }
-            Storage::disk('local')->put('post-temp/'.$user->username.'-'.$user->id.'/temp.'.$ext, file_get_contents($fileContents));
+            Storage::disk('local')->put('app/post-temp/'.$user->username.'-'.$user->id.'/temp.'.$ext, file_get_contents($fileContents));
             $photo = storage_path('post-temp/'.$user->username.'-'.$user->id).'/temp.'.$ext;
           }
           else {
