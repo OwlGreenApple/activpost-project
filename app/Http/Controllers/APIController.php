79598c2e11cc5b8ee550d243f5c6e5e96c9080bc
rizky redjo <celebgramme.dev@gmail.com>
2019-02-19 10:28:06 +0700

    fixing

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -69,8 +69,10 @@ class APIController extends Controller
           if ($sc->is_s3) {
             $fileContents = Storage::disk('s3')->url($sc->image);
             $ext = substr(strrchr($fileContents,'.'),1);
+            // if (!file_exists($dir)) {
+            // }
             Storage::disk('local')->put('post-temp/'.$user->username.'-'.$user->id.'/temp.'.$ext, file_get_contents($fileContents));
-            $photo = storage_path('post-temp/'.$user->username.'-'.$user->id.'/temp.'.$ext);
+            $photo = storage_path('post-temp/'.$user->username.'-'.$user->id).'/temp.'.$ext;
           }
           else {
             $dir = base_path('../public_html/vp/uploads/'.$user->username.'-'.$user->id); 
