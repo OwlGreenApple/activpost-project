750f5662bd1ba1f808ebbcc88ac030c801899cee
rizky redjo <celebgramme.dev@gmail.com>
2019-02-19 15:58:07 +0700

    fixing

---
+++ database/migrations/2018_12_04_111609_add_is_s3_to_schedules_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIsS3ToSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('schedules', function (Blueprint $table) {
+            $table->boolean('is_s3')->default(0);
+        });
+        Schema::table('images', function (Blueprint $table) {
+            $table->boolean('is_s3')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('schedules', function (Blueprint $table) {
+            //
+        });
+    }
+}
