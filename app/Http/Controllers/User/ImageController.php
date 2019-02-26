2859d16267cc6c12d69e6e37478139131b5addf9
rizky redjo <celebgramme.dev@gmail.com>
2019-02-26 15:26:22 +0700

    fixing

--- app/Http/Controllers/User/ImageController.php
+++ app/Http/Controllers/User/ImageController.php
@@ -106,20 +106,13 @@ class ImageController extends Controller
 			// file_put_contents($img, file_get_contents($url));			
       $urls3 = Storage::disk('s3')->putFile($dir, file_get_contents($url),'public');
 		} else if (Request::input("decryptData") == "0"){
-			// Image::make(Request::input("imgData"))->save($dir."/".$filename.".jpg");
-      // $image_parts = explode(";base64,", Request::input("imgData"));
-      // $image_base64 = $image_parts[1];
-      // $encodedData = str_replace(' ','+',$image_base64);
-      // $decocedData = base64_decode($encodedData);
-      // $urls3 = Storage::disk('s3')->putFile($dir, file_get_contents($decocedData),'public');
+      $dirLocal = public_path('../vp/users/'.$user->username.'-'.$user->id); 
+      if (!file_exists($dirLocal)) {
+        mkdir($dirLocal,0741,true);
+      }
+			Image::make(Request::input("imgData"))->save($dirLocal."/".$filename.".jpg");
       
-      $image = Request::input("imgData");  // your base64 encoded
-      $image = str_replace('data:image/jpeg;base64,', '', $image);
-      $image = str_replace(' ', '+', $image);      
-
-      //decode base64 string
-      $image = base64_decode($base64_str);
-      $urls3 = Storage::disk('s3')->putFile($dir, base64_decode($image),'public');
+      $urls3 = Storage::disk('s3')->putFile($dir, new File($dirLocal."/".$filename.".jpg"),'public');
 		}
 		$imageM = new ImageModel;
 		$imageM->is_schedule = 0;
