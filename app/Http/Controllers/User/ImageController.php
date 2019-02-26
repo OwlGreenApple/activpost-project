481492cb5f195f0b22ee730e863ed2a183de733f
rizky redjo <celebgramme.dev@gmail.com>
2019-02-26 13:30:53 +0700

    fixing data

--- app/Http/Controllers/User/ImageController.php
+++ app/Http/Controllers/User/ImageController.php
@@ -107,7 +107,7 @@ class ImageController extends Controller
       $urls3 = Storage::disk('s3')->putFile($dir, file_get_contents($url),'public');
 		} else if (Request::input("decryptData") == "0"){
 			// Image::make(Request::input("imgData"))->save($dir."/".$filename.".jpg");
-      $urls3 = Storage::disk('s3')->putFile($dir, Request::input("imgData"),'public');
+      $urls3 = Storage::disk('s3')->putFile($dir, Request::input("imgData2"),'public');
 		}
 		$imageM = new ImageModel;
 		$imageM->is_schedule = 0;
