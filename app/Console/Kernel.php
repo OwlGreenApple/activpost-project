1a90e1794fdd2678c31137a2703adf2264f6b262
rizky redjo <celebgramme.dev@gmail.com>
2019-03-15 11:54:23 +0700

    fixing

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,6 +30,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+      $schedule->command('count:userstime')->cron('0 */2 * * *')->withoutOverlapping();
       /*
 			// $schedule->command('send:instagram')->withoutOverlapping()->timezone(''.env('IG_TIMEZONE').'');
       $schedule->command('send:instagram')->timezone(''.env('IG_TIMEZONE').'');
