3e38c64665aafda9e262b5170f0a14d8d080f0f8
rizky redjo <celebgramme.dev@gmail.com>
2019-10-10 09:27:46 +0700

    trying to fixing

--- app/Http/Controllers/User/ScheduleController.php
+++ app/Http/Controllers/User/ScheduleController.php
@@ -517,7 +517,8 @@ class ScheduleController extends Controller
 			$uptogd = $name . '.' . $extension;
 			// $uri = asset('images/uploads/'.$uptogd);
 			// $uri = asset('../vp/uploads/'.$uptogd);
-			$uri = url('../vp/uploads/'.$uptogd);
+			// $uri = url('../vp/uploads/'.$uptogd);
+			$uri = '127.0.0.1/vp/uploads/'.$uptogd;
 			// Update
 			$schedule = Schedule::findOrFail($request->id);
 			$schedule->image = $uri;
